67f6fd7497d81fcd63bdce77dc72f6b14f9b3652
Kage-Labs <mst.unknown100@gmail.com>
2025-12-20 23:02:13 +0100

    Organize Java fundamentals with proper package naming

---
+++ Fundamentals/Variables/Variables.java
@@ -0,0 +1,23 @@
+package Fundamentals.Variables;
+
+public class Variables {
+      public static void main(String[] args){
+          // --- DATA TYPES (Storage Boxes) ---
+
+    // String: Stores text (must be in double quotes)
+    String Name = "Kage";
+
+    //int:Stores whole numbers (no decimals)
+    int age = 18;
+
+    //double: Stores numbers with decimals (precise values)
+    double height = 1.83;
+
+    //boolean: Stores a simple true or false value
+    boolean isStudent = true;
+      System.out.println("Name:" + Name);
+      System.out.println("Age:" + age);
+      System.out.println("Height:" + height + " metres");
+      System.out.println("Student:" + isStudent);
+      }
+}
